50ae203e319a5b4b06b1ba4fbf2f044963a9d251
alessio123 <andreaalessioenzo@alice.it>
2015-04-15 17:31:33 +0200

    Update hw.java

--- src/hw.java
+++ src/hw.java
@@ -1,7 +1,7 @@
 
 public class hw {
 	 public static void main(String[] args) {
-	        System.out.println("Hello World!!! Auddino Alessio 5B 11/4/2015");
+	        System.out.println("Ciao Mondo!!! Auddino Alessio 5B 11/4/2015");
 	    }
 
 }
